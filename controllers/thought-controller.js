86e6f4b82145dc123bfd8ed233a931771b11c819
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-06-08 19:57:42 -0400

    added to user controller for GET and POST

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,9 @@
 const res = require('express/lib/response');
 const { User, Thought } = require('../models');
 
+const thoughtController = {
+    addThought({ params, body }, res) {
+        console.log(body)
+        Thought.create(body)
+    }
+}
